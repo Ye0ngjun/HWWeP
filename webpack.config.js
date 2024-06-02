f657f533924282d4e8c69f1a18ec0f1065460171
Yeongjun Joo <wndudwns6824@naver.com>
2024-06-03 01:15:07 +0900

    수정

--- webpack.config.js
+++ webpack.config.js
@@ -5,49 +5,50 @@ module.exports = {
   mode: "development",
   entry: "./src/",
   output: {
-	path: path.resolve(__dirname, "build"),
-	filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
   },
   resolve: {
-	modules: [path.join(__dirname, "src"), "node_modules"],
-	alias: {
-		react: path.join(__dirname, "node_modules", "react"),
-	},
+    extensions: [".js", ".jsx"],
+    modules: [path.join(__dirname, "src"), "node_modules"],
+    alias: {
+      react: path.join(__dirname, "node_modules", "react"),
+    },
   },
   module: {
-	rules: [
-	  {
-		test: /\.(js|jsx)$/,
-		exclude: "/node_modules",
-		use: {
-		  loader: "babel-loader",
-		},
-	  },
-	  {
-		test: /\.css$/,
-		use: [
-		  {
-			loader: "style-loader",
-		  },
-		  {
-			loader: "css-loader",
-		  },
-		],
-	  },
-	  {
-		test: /\.html$/,
-		use: [
-		  {
-			loader: "html-loader",
-			options: { minimize: true }
-		  }
-		]
-	  }
-	],
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: "/node_modules",
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-	new HtmlWebPackPlugin({
-	  template: './src/index.html'
-	})
-  ]
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
